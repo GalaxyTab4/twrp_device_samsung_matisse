fd9e8aba82559d946f2c10e23519e19a69199ff2
sub77 <sub77@ymail.com>
2015-10-29 22:23:48 +0100

    update

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -17,7 +17,7 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
 		$(LZMA_RAMDISK) \
 		$(recovery_kernel)
 	@echo -e ${CL_CYN}"----- Making recovery image ------"${CL_RST}
-	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) -dt $(LOCAL_PATH)/kernel/dt.img --output $@ --ramdisk $(LZMA_RAMDISK)
+	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(LOCAL_PATH)/kernel/dt.img --output $@ --ramdisk $(LZMA_RAMDISK)
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
 	@echo -e ${CL_CYN}"Made recovery image: $@"${CL_RST}
 
